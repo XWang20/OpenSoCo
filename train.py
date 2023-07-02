85a7f483ad671ec3022c7f3022606037609bc77f
xing <wangxing1027@gmail.com>
2023-07-02 15:15:43 +0800

    change start step

--- train.py
+++ train.py
@@ -60,24 +60,21 @@ def get_optimizer(args, model):
     if args.save is not None:
         bmp.print_rank("Loading the optimizer...")
         
-        # if use the momentum, load optimizer
-        states = torch.load(
-            os.path.join(args.save, 'checkpoints', "checkpoint.rank-%d.opt" % (bmp.rank())))
+        # # if use the momentum, load optimizer
+        # states = torch.load(
+        #     os.path.join(args.save, 'checkpoints', "checkpoint.rank-%d.opt" % (bmp.rank())))
         
-        # if use the momentum, load the "state" in the optimizer state_dict
-        optimizer.load_state_dict(states)
+        # # if use the momentum, load the "state" in the optimizer state_dict
+        # optimizer.load_state_dict(states)
         
         # if dont use the momentum, delete the "state" in the optimizer state_dict
-        # states = torch.load(
-        #     os.path.join(args.save, 'optimizers', "optimizer-%d.rank-%d.opt" % (args.start_step, 0)))
+        states = torch.load(
+            os.path.join(args.save, 'checkpoints', "optimizer.rank-%d.opt" % (bmp.rank())))
 
-        # states = torch.load(
-        #     os.path.join(args.save, 'checkpoints', "optimizer.rank-%d.opt" % (bmp.rank())))
-
-        # del states['state']
-        # optimizer_state = optimizer.state_dict()
-        # optimizer_state.update(states)
-        # optimizer.load_state_dict(optimizer_state)
+        del states['state']
+        optimizer_state = optimizer.state_dict()
+        optimizer_state.update(states)
+        optimizer.load_state_dict(optimizer_state)
 
         for name, param in optimizer.state_dict().items():
             if name == "param_groups":
