360786ac298631a6ee02f44fefc1df64247905d0
xing <wangxing1027@gmail.com>
2023-07-03 17:59:35 +0800

    fix upload hdfs bash

--- scripts/upload_hdfs.sh
+++ scripts/upload_hdfs.sh
@@ -1,1 +1,4 @@
+export HADOOP_USER_NAME=tc_agi
+export HADOOP_USER_PASSWORD=IH2U3AS1D
+
 hdfs dfs -put /data/checkpoints/checkpoint-392500.pt /user/tc_agi/user/wangxing/save/deberta_prenorm_OpenSoCo_en/1e-4-init-embed/checkpoints/checkpoint-392500.pt
