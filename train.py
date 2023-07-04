f06db74df60d14b293a14d13777ebbf4f95d06aa
xing <wangxing1027@gmail.com>
2023-07-04 16:26:32 +0800

    change st

--- train.py
+++ train.py
@@ -136,7 +136,7 @@ def setup_model_and_optimizer(args):
     # get the optimizer and lr_scheduler
     optimizer = get_optimizer(args, model)
     lr_scheduler = get_learning_rate_scheduler(args, optimizer)
-    optimizer, lr_scheduler = lower_learning_rate(args, model, lr_scheduler, scale_factor=0.8)
+    optimizer, lr_scheduler = lower_learning_rate(args, model, lr_scheduler, scale_factor=0.6)
     optim_manager = get_optim_manager(args, optimizer, lr_scheduler)
     bmp.synchronize()
     # get the memory usage
@@ -420,7 +420,7 @@ def main():
     # if last_step > args.start_step:
     #     args.start_step = last_step
 
-    args.start_step = 352500
+    args.start_step = 385000
 
     # init wandb and tensorboard
     if args.report_to == "wandb":
