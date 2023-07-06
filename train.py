2790c449886ee795cb149a61457339a6784c4ec7
xing <wangxing1027@gmail.com>
2023-07-06 10:15:37 +0800

    change st

--- train.py
+++ train.py
@@ -432,7 +432,7 @@ def main():
     # if last_step > args.start_step:
     #     args.start_step = last_step
 
-    args.start_step = 392500
+    args.start_step = 395000
 
     # init wandb and tensorboard
     if args.report_to == "wandb":
