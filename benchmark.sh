3b4d45944bea0455a7d2c4d30f4eadd5d87b1524
xing <wangxing1027@gmail.com>
2023-07-10 02:05:08 +0800

    add test

---
+++ benchmark.sh
@@ -0,0 +1,6 @@
+export NCCL_P2P_DISABLE=1
+export CUDA_LAUNCH_BLOCKING=1
+
+pip install -v -e ./bmtrain
+
+torchrun --nnodes=1 --nproc_per_node=4 --rdzv_id=1 --rdzv_backend=c10d --rdzv_endpoint=localhost bmtrain/example/benchmark.py
