fbbf96dab9f3e6bf85fd630820ff61c1bafa2ccd
xing <wangxing1027@gmail.com>
2023-07-11 00:16:45 +0800

    fix bf16

--- deberta_jeeves_bf16.sh
+++ deberta_jeeves_bf16.sh
@@ -9,13 +9,11 @@ export NCCL_ASYNC_ERROR_HANDLING=1
 
 export CUDA_LAUNCH_BLOCKING=1
 
-# pip install model_center==0.1.3
-
 pip install model_center
 
 ls -l -ch /data
 
-GPUS_PER_NODE=4
+GPUS_PER_NODE=8
 
 if [ ${IDC} == klara-2-pek02 ]; then
     DISTRIBUTED_ARGS="--nnodes=${WORLD_SIZE} \
