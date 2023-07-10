8888fa18c4f3476ce26fde5c9da60d9a8ab9975b
xing <wangxing1027@gmail.com>
2023-07-10 12:27:37 +0800

    fix bf16

--- deberta_jeeves_bf16.sh
+++ deberta_jeeves_bf16.sh
@@ -3,7 +3,7 @@
 export NCCL_P2P_DISABLE=1
 export CUDA_LAUNCH_BLOCKING=1
 
-pip install -v -e ./bmtrain
+# pip install -v -e ./bmtrain
 
 pip install model_center==0.1.3
 ls /mnt/data/user/tc_agi/user/wangxing
