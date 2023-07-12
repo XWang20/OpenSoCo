64dde2f35f4f99d2d89c350ae09924c24cb37e98
xing <wangxing1027@gmail.com>
2023-07-12 19:21:51 +0800

    wirte tensorboard

--- move_tensorboard.sh
+++ move_tensorboard.sh
@@ -1,1 +1,1 @@
-cp log_file/tensorboard/events.out.tfevents.1689160451.real-107.1083292.0 /data/tensorboard/tensorboard/0/20230711231242/
+cp log_file/tensorboard/events.out.tfevents.1689160451.real-107.1083292.0 /data/tensorboard/0/20230711231242/
