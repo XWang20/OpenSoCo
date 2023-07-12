d346b64142207a44153f78f48d98c996d888f95d
xing <wangxing1027@gmail.com>
2023-07-12 19:20:21 +0800

    wirte tensorboard

---
+++ move_tensorboard.sh
@@ -0,0 +1,1 @@
+cp log_file/tensorboard/events.out.tfevents.1689160451.real-107.1083292.0 /data/tensorboard/tensorboard/0/20230711231242/
--- train.py
+++ train.py
@@ -218,7 +218,6 @@ def valid(args, model, dev_dataloader, loss_func, step, writer):
             input_ids, attention_mask, labels = input_ids.cuda(), attention_mask.cuda(), labels.cuda()
             logits = model(input_ids=input_ids, attention_mask=attention_mask, return_logits=True)
             loss = loss_func(logits.view(-1, logits.shape[-1]), labels.view(-1))
-            # bmp.print_rank(f"step: {step} | batch_size: {input_ids.size()} | loss: {loss}")
             global_loss = bmp.sum_loss(loss).item()
             valid_loss += global_loss
 
