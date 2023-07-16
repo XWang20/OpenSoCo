30e8140d91484e544ffce2342b35b8c4a5a9ddd4
xing <wangxing1027@gmail.com>
2023-07-16 17:39:17 +0800

    change st

--- train.py
+++ train.py
@@ -247,7 +247,7 @@ def batch_iter(args, dataset):
     # 英文模型
     # st = 0  # 从第一个数据开始训练
     # st = (args.start_step + 90000 - 357500) * args.batch_size
-    st = 90000 * args.batch_size * 2 + args.start_step * args.batch_size
+    st = 70000 * args.batch_size
     # st = 0
     input_ids_list = []
     attention_mask_list = []
