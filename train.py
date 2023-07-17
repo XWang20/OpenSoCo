a1492a02446a72c7a3bbb30d322a41f36c07e24f
xing <wangxing1027@gmail.com>
2023-07-17 22:55:36 +0800

    add check model param

--- train.py
+++ train.py
@@ -499,7 +499,7 @@ def main():
     last_step = get_last_step(args, args.start_step)
     if last_step > args.start_step:
         args.start_step = last_step
-    args.start_step = 232500
+    args.start_step = 237500
 
     # init wandb and tensorboard
     if args.report_to == "wandb":
