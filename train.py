99fb0d005fe4282bbfd621c601e3f332cf9b2208
xing <wangxing1027@gmail.com>
2023-07-19 17:34:11 +0800

    fix bf16

--- train.py
+++ train.py
@@ -486,7 +486,7 @@ def initialize():
     args = get_args()
     # init bmp 
     bmp.print_rank("Init bmp distributed.")
-    bmp.init_distributed(seed=args.seed, zero_level=3)
+    # bmp.init_distributed(seed=args.seed, zero_level=3)
     
     bmp.init_distributed(seed=args.seed)
 
