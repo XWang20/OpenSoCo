fd91260c34d9fa261283fa19134965bb8d4575ab
xing <wangxing1027@gmail.com>
2023-07-19 18:40:35 +0800

    add model center

--- roberta.py
+++ roberta.py
@@ -47,8 +47,8 @@ class RoertaLMHead(torch.nn.Module):
         print(f"rank: {bmp.rank()} | start projection")
         logits = input_embedding.projection(hidden_states)
         print(f"rank: {bmp.rank()} | get bias")
-        bias = self.decoder.bias
-        print(f"rank: {bmp.rank()} | add bias")
+        # bias = self.decoder.bias
+        # print(f"rank: {bmp.rank()} | add bias")
         # logits = logits + bias
         print(f"rank: {bmp.rank()} | start return logits")
         return logits
