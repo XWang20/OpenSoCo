f497d0e5a62b2236dc008c58541480dfc549f9c5
xing <wangxing1027@gmail.com>
2023-07-20 23:00:45 +0800

    change st

--- train_bf16.py
+++ train_bf16.py
@@ -84,13 +84,13 @@ def get_optimizer(args, model):
             os.path.join(args.save, 'checkpoints', "checkpoint.rank-%d.opt" % (bmp.rank())))
         
         # if use the momentum, load the "state" in the optimizer state_dict
-        # optimizer.load_state_dict(states)
+        optimizer.load_state_dict(states)
         
         # if dont use the momentum, delete the "state" in the optimizer state_dict
-        del states['state']
-        optimizer_state = optimizer.state_dict()
-        optimizer_state.update(states)
-        optimizer.load_state_dict(optimizer_state)
+        # del states['state']
+        # optimizer_state = optimizer.state_dict()
+        # optimizer_state.update(states)
+        # optimizer.load_state_dict(optimizer_state)
 
         for name, param in optimizer.state_dict().items():
             if name == "param_groups":
@@ -422,7 +422,6 @@ def main():
     last_step = get_last_step(args, args.start_step)
     if last_step > args.start_step:
         args.start_step = last_step
-    args.start_step = 413000
 
     # init wandb and tensorboard
     if args.report_to == "wandb":
